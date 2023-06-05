2afe217c7faa2493f0d03793c1a849c4bbaaed11
Aldo Fiore <aldo.fiore95@gmail.com>
2023-06-05 17:32:56 +0200

    Clarifying why the code is doing something in a confusing part

--- src/reducer.ts
+++ src/reducer.ts
@@ -19,7 +19,10 @@ export function gridReducer(state: State, action: Actions) {
       const newToFill: string[] = [];
       const newGrid = state.grid.map((row, rowIndex) =>
         row.map((value, columnIndex) => {
+          // update the value only if you are in the selected row if you are
+          // looping through another row but you are in the correct column
           if (columnIndex === y || rowIndex === x) {
+            // keep track of what needs to be filled yellow
             newToFill.push(`${rowIndex}-${columnIndex}`);
             return value + 1;
           }
